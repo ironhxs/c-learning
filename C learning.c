5b19cdd96724ec909c3335f965da85241afccb28
贺鑫帅 <3077066784@qq.com>
2023-07-26 18:45:11 +0800

    3的倍数

---
+++ C learning.c
@@ -0,0 +1,10 @@
+
+#include "C learning.h"
+#include <stdio.h>
+
+int main(void ) {
+    int i =1;
+    for(i=1;i<=100;i++)    {if(i%3==0)
+        printf ("%d\n" ,i );
+        };
+    return 0;
---
+++ C learning.h
@@ -0,0 +1,13 @@
+//
+//  C learning.h
+//  
+//
+//  Created by 贺鑫帅 on 2023/7/26.
+//
+
+#ifndef C_learning_h
+#define C_learning_h
+
+#include <stdio.h>
+
+#endif /* C_learning_h */
